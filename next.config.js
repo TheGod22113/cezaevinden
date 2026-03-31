298533037220a6f08c2e4b7d294dbef149e684dd
halil kosger <aysberk.mk@gmail.com>
2026-03-31 09:17:47 +0300

    fix: remove www redirect causing redirect loop

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com', 'ui-avatars.com'],
   },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.cezaevinden.com' }],
-        destination: 'https://cezaevinden.com/:path*',
-        permanent: true,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
