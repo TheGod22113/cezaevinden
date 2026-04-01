9220b8bad98ace81558230f085426e2e65a181a0
halil kosger <aysberk.mk@gmail.com>
2026-04-01 12:56:39 +0300

    feat: generateMetadata, sitemap, robots, 404 dark mode

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,33 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/admin/', '/api/', '/mesajlar/', '/profil/', '/giris', '/kayit', '/bildirimler'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/admin/',
+          '/api/',
+          '/mesajlar/',
+          '/profil/',
+          '/giris',
+          '/kayit',
+          '/bildirimler',
+          '/arama',
+          '/kaydedilenler',
+          '/onboarding',
+          '/profil/ayarlar',
+        ],
+      },
+      {
+        userAgent: 'GPTBot',
+        disallow: ['/'],
+      },
+      {
+        userAgent: 'CCBot',
+        disallow: ['/'],
+      },
+    ],
     sitemap: 'https://cezaevinden.com/sitemap.xml',
   }
 }
